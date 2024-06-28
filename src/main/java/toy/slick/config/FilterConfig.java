031effa70a5e7ddc2d5076605ae8b58190eae2cf
wjdwo951219 <wjdwo951219@gmail.com>
2024-06-28 09:24:45 +0900

    add RequestLoggingFilter

---
+++ src/main/java/toy/slick/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package toy.slick.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import toy.slick.filter.RequestLoggingFilter;
+
+@Configuration
+public class FilterConfig {
+    // TODO: X-API-KEY
+    // TODO: Bucket4j
+
+    @Bean
+    public RequestLoggingFilter requestLoggingFilter() {
+        RequestLoggingFilter filter = new RequestLoggingFilter();
+
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeQueryString(true);
+
+        return filter;
+    }
+}
